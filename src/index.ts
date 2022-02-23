c45935adfd764f230f4d9ea9491ec4f83ca11d02
ZenShibata <zenshibata@outlook.com>
2022-02-23 10:55:24 +0700

    feat(i18n): add en-US

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import Collection from "@discordjs/collection";
+import { readdir } from "fs/promises";
+import { resolve } from "path";
+import enUS from "./localization/en-US.js";
+
+export type DefaultLang = typeof enUS;
+export type LangID = ReturnType<DefaultLang["META_ID"]>;
+export class Localization extends Collection<LangID, DefaultLang> {
+    public readonly default = enUS;
+    public constructor(public langID: LangID) { super(); }
+
+    public async load(): Promise<any> {
+        const langsFolderPath = resolve(__dirname, "..", "langs");
+        const langsFile = await readdir(langsFolderPath);
+        for (const langFile of langsFile) {
+            const lang: DefaultLang = await import(resolve(langsFolderPath, langFile));
+            this.set(lang.META_ID(), lang);
+        }
+    }
+
+    public get lang(): DefaultLang {
+        return this.get(this.langID) ?? this.default;
+    }
+}
---
+++ src/localization/en-US.ts
@@ -0,0 +1,6 @@
+const lang = {
+    META_ID: () => "en-US",
+    RAJAT_KONTOL: () => "Rajat gay"
+};
+
+export default lang;
